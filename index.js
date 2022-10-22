de9523eda1c825dab442d18afbf6d3eb2f4ba991
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-10-22 20:44:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+const categories = require('./data/category.json');
+
+app.get('/', (req, res) => {
+    res.send('News Api Running')
+
+})
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('Dragon News Server Running On port', port);
+})
